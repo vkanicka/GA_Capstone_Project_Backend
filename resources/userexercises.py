ecc9d152fdec2d3bf268e1121cfb2301ff8c4fd9
VICTORIA_KANICKA <vkanicka@gmail.com>
2021-05-21 10:51:35 -0500

    User exercise show and update routes working

--- resources/userexercises.py
+++ resources/userexercises.py
@@ -18,7 +18,7 @@ userexercises = Blueprint('userexercises','userexercises')
 #--------------------------------------------
 @userexercises.route('/', methods=['GET'])
 def userexercises_index():
-    result = models.UserExercise.select()
+    result = current_user.this_users_exercises
     userexercises_dicts = [model_to_dict(userexercises) for userexercises in result]
     return jsonify({
         'data': userexercises_dicts,
@@ -33,6 +33,7 @@ def userexercises_index():
 def add(exercise_id):
 # GET INPUT
     payload = request.get_json()
+    # MAY WANT TO ADD IF USEREXERCISE DOES NOT ALREADY EXIST, CREATE, ELSE RETURN ERROR MESSAGE
     new_exercise = models.UserExercise.create(
     user= current_user.id,
     exercise = exercise_id,
@@ -47,3 +48,29 @@ def add(exercise_id):
         message='Successfully add exercise to user!',
         status=201
     ), 201
+
+#--------------------------------------------
+# SHOW USER EXERCISE
+#--------------------------------------------
+@userexercises.route('/<user_exercise_id>', methods=["GET"])
+def get_one_user_exercise(user_exercise_id):
+    user_exercise = models.UserExercise.get_by_id(user_exercise_id)
+    return jsonify(
+    data = model_to_dict(user_exercise),
+    message = 'Success!!',
+    status = 200
+    ), 200
+
+
+#--------------------------------------------
+# UPDATE USER EXERCISE
+#--------------------------------------------
+@userexercises.route('/<user_exercise_id>', methods=["PUT"])
+def update_user_exercise(user_exercise_id):
+    payload = request.get_json()
+    models.UserExercise.update(**payload).where(models.UserExercise.id==user_exercise_id).execute()
+    return jsonify(
+        data = model_to_dict(models.UserExercise.get_by_id(user_exercise_id)),
+        status = 200,
+        message = 'resource updated successfully'
+    ), 200
