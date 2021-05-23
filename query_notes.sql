59e20f4f3f159b840d7d18c4c06ee4fe152d7498
VICTORIA_KANICKA <vkanicka@gmail.com>
2021-05-23 14:05:07 -0500

    Finished thought and behavior seeds. Finished SQL query tying ETBs to Exercise example. Next need to figure out how to store input form data.

--- query_notes.sql
+++ query_notes.sql
@@ -1,10 +1,29 @@
-SELECT T.id, T.tag, COUNT(E.emotion) AS count
-from emotion e
-LEFT JOIN emotiontags ET ON E.id=ET.emotion_id LEFT JOIN tag T ON T.id = ET.tag_id
-WHERE E.id IN (4, 5, 2)
-GROUP BY T.tag
-ORDER BY COUNT DESC;
+SELECT TAG, SUM(COUNT) AS TOTAL FROM
+(
+  SELECT tag.tag AS TAG, COUNT(emotion.emotion) AS COUNT
+  from emotion
+  LEFT JOIN emotiontags ON emotion.id=emotiontags.emotion_id LEFT JOIN tag ON tag.id = emotiontags.tag_id
+  WHERE emotion.id IN (4, 5, 2)
+  GROUP BY tag.tag
 
--- id|tag|count
--- 1|Time Management|2
--- 6|Depression|1
+  UNION ALL
+
+  SELECT tag.tag AS TAG, COUNT(thought.thought) AS COUNT
+  from thought
+  LEFT JOIN thoughttags ON thought.id=thoughttags.thought_id
+  LEFT JOIN tag ON tag.id = thoughttags.tag_id
+  WHERE thought.id IN (1, 2, 4, 6)
+  GROUP BY tag.tag
+
+  UNION ALL
+
+  SELECT tag.tag AS TAG, COUNT(behavior.behavior) AS COUNT
+  from behavior
+  LEFT JOIN behaviortags ON behavior.id=behaviortags.behavior_id
+  LEFT JOIN tag ON tag.id = behaviortags.tag_id
+  WHERE behavior.id IN (1)
+  GROUP BY tag.tag
+)
+GROUP BY (TAG)
+ORDER BY TOTAL DESC
+;
