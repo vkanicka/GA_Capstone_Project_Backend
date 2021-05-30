2fa3eee290a762bcb2e2fcb7a275e9dcc57fd73d
VICTORIA_KANICKA <vkanicka@gmail.com>
2021-05-29 20:49:51 -0500

    Postman is linking input form submitted on front end to database and sending back suggested exercise. Database currently only has tag mapping for time management exercise. Next step is to map a couple other examples to ensure this is working correctly. Then will create a fetch post on the input form submit button and display component with suggested exercise.

--- exercise_mapping_for_debugging.py
+++ exercise_mapping_for_debugging.py
@@ -2,15 +2,14 @@ import sqlite3
 con = sqlite3.connect('capstone.sqlite')
 cur = con.cursor()
 
-suggested_exercise_query_result = cur.execute('''
-SELECT EXERCISE.NAME, EXERCISE.DESCRIPTION FROM (
+exercise_mapping_query = cur.execute('''
+SELECT EXERCISE.NAME, EXERCISE.DESCRIPTION, TAG FROM (
 
   SELECT ID AS TAG_ID, TAG, SUM(COUNT) AS TOTAL FROM
   (
     SELECT tag.id AS ID, tag.tag AS TAG, COUNT(emotion.emotion) AS COUNT
     from emotion
     LEFT JOIN emotiontags ON emotion.id=emotiontags.emotion_id LEFT JOIN tag ON tag.id = emotiontags.tag_id
-    WHERE emotion.status = 1
     GROUP BY tag.tag
 
     UNION ALL
@@ -19,7 +18,6 @@ SELECT EXERCISE.NAME, EXERCISE.DESCRIPTION FROM (
     from thought
     LEFT JOIN thoughttags ON thought.id=thoughttags.thought_id
     LEFT JOIN tag ON tag.id = thoughttags.tag_id
-    WHERE thought.status = 1
     GROUP BY tag.tag
 
     UNION ALL
@@ -28,18 +26,17 @@ SELECT EXERCISE.NAME, EXERCISE.DESCRIPTION FROM (
     from behavior
     LEFT JOIN behaviortags ON behavior.id=behaviortags.behavior_id
     LEFT JOIN tag ON tag.id = behaviortags.tag_id
-    WHERE behavior.status = 1
     GROUP BY tag.tag
   )
 
   GROUP BY (TAG)
-  ORDER BY TOTAL DESC
-  LIMIT 1
 
 ) AS FOUND_TAG
 LEFT JOIN exercisetags ON exercisetags.tag_id = FOUND_TAG.tag_id
 LEFT JOIN exercise ON exercise.id = exercisetags.exercise_id
 ;
-''').fetchone()
+''').fetchall()
+
+print(exercise_mapping_query)
 
 con.close()
