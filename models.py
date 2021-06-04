6c6859cdac9dcdaf4c41c21e3692a6868b95c8c6
VICTORIA_KANICKA <vkanicka@gmail.com>
2021-06-03 20:15:43 -0500

    took off truncate options

--- models.py
+++ models.py
@@ -171,7 +171,7 @@ def initialize():
     tables = [User, Exercise, SuggestedExercise, UserExercise, Tag, ExerciseTags, Emotion, Thought, Behavior, EmotionTags, ThoughtTags, BehaviorTags]
     for table in tables:
         # DATABASE.drop_tables([table], cascade=True)
-        DATABASE.truncate_table([table],restart_identity,cascade)
+        DATABASE.truncate_table([table])
         DATABASE.create_tables([table], safe=True)
     # DATABASE.create_tables([User, Exercise, SuggestedExercise, UserExercise, Tag, ExerciseTags, Emotion, Thought, Behavior, EmotionTags, ThoughtTags, BehaviorTags], safe=True)
     print("Initialized database and tables...")
