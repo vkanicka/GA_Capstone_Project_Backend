b02b8568ec424c10b23af59155e196c32961bc26
VICTORIA_KANICKA <vkanicka@gmail.com>
2021-06-04 02:22:50 -0500

    commented out drop tables for heroku

--- models.py
+++ models.py
@@ -170,8 +170,7 @@ def initialize():
     DATABASE.connect()
     tables = [User, Exercise, SuggestedExercise, UserExercise, Tag, ExerciseTags, Emotion, Thought, Behavior, EmotionTags, ThoughtTags, BehaviorTags]
     for table in tables:
-        DATABASE.drop_tables([table], on_delete='CASCADE')
-
+        # DATABASE.drop_tables([table], on_delete='CASCADE')
         # DATABASE.truncate_table([table])
         DATABASE.create_tables([table], safe=True)
     # DATABASE.create_tables([User, Exercise, SuggestedExercise, UserExercise, Tag, ExerciseTags, Emotion, Thought, Behavior, EmotionTags, ThoughtTags, BehaviorTags], safe=True)
